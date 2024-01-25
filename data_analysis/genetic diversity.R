5cc427bd3105617a43b774018bc9c11bd4167514
LinaAoyama <laoyama@uoregon.edu>
2024-01-25 11:05:04 -0800

    allele freq graph

--- data_analysis/genetic diversity.R
+++ data_analysis/genetic diversity.R
@@ -13,25 +13,48 @@ column_class(genomic_data, class = "locus")
 
 #visualize allele frequencies
 plot(genomic_data$Loc025) #84, 87, 141, 144, 147, 150, 153, 156, 160, 164, 167
-ggplot() + geom_locus(aes(x = Loc025, fill = Treatment), data = genomic_data) #Anatone: 144, 150 OK
+L025 <- ggplot() + geom_locus(aes(x = Loc025, fill = Treatment), data = genomic_data)+
+  annotate("text", label = "L025", x = 2, y = 1, size = 5)+
+  ylim(c(0,1))#Anatone: 144, 150 OK
 plot(genomic_data$Loc040) #169, 172, 175, 179, 182, 185, 188, 191, 194, 197
-ggplot() + geom_locus(aes(x = Loc040, fill = Treatment), data = genomic_data) #Anatone: 182, 188, 191 OK
+L040 <- ggplot() + geom_locus(aes(x = Loc040, fill = Treatment), data = genomic_data)+ 
+  annotate("text", label = "L040", x = 2, y = 1, size = 5)+
+  ylim(c(0,1))#Anatone: 182, 188, 191 OK
 plot(genomic_data$Loc209) #167, 170, 173, 179, 182, 185, 188, 191, 196, 199
-ggplot() + geom_locus(aes(x = Loc209, fill = Treatment), data = genomic_data) #Anatone: 182 OK
+L209 <- ggplot() + geom_locus(aes(x = Loc209, fill = Treatment), data = genomic_data)+
+  annotate("text", label = "L209", x = 2, y = 1, size = 5)+
+  ylim(c(0,1))#Anatone: 182 OK
 plot(genomic_data$Loc262) #473, 476, 479, 482, 485, 488
-ggplot() + geom_locus(aes(x = Loc262, fill = Treatment), data = genomic_data) #Anatone: 225, 292 Weird
+L262 <- ggplot() + geom_locus(aes(x = Loc262, fill = Treatment), data = genomic_data)+
+  annotate("text", label = "L262", x = 2, y = 1, size = 5)+
+  ylim(c(0,1))#Anatone: 225, 292 Weird
 plot(genomic_data$Loc307) #162, 165, 168, 171, 174, 177, 197
-ggplot() + geom_locus(aes(x = Loc307, fill = Treatment), data = genomic_data) #Anatone: 177 OK
+L307 <- ggplot() + geom_locus(aes(x = Loc307, fill = Treatment), data = genomic_data)+
+  annotate("text", label = "L307", x = 2, y = 1, size = 5)+
+  ylim(c(0,1))#Anatone: 177 OK
 plot(genomic_data$Loc338) #169, 172, 175, 179, 182, 185, 188, 192, 206
-ggplot() + geom_locus(aes(x = Loc338, fill = Treatment), data = genomic_data) #Anatone: 175, 182 Weird
+L338 <- ggplot() + geom_locus(aes(x = Loc338, fill = Treatment), data = genomic_data)+
+  annotate("text", label = "L338", x = 2, y = 1, size = 5)+
+  ylim(c(0,1))#Anatone: 175, 182 Weird
 plot(genomic_data$Loc396) #164, 177, 183, 228, 231, 234, 237, 240
-ggplot() + geom_locus(aes(x = Loc396, fill = Treatment), data = genomic_data) #Anatone: 164, 228, 231 Weird
+L396 <- ggplot() + geom_locus(aes(x = Loc396, fill = Treatment), data = genomic_data)+
+  annotate("text", label = "L396", x = 2, y = 1, size = 5)+
+  ylim(c(0,1))#Anatone: 164, 228, 231 Weird
 plot(genomic_data$Loc548) #39, 71, 84, 92, 95, 98, 101, 104, 107, 110, 113, 116, 119, 122, 125, 128, 132
-ggplot() + geom_locus(aes(x = Loc548, fill = Treatment), data = genomic_data) #Anatone: 84 Weird
+L548 <- ggplot() + geom_locus(aes(x = Loc548, fill = Treatment), data = genomic_data)+
+  annotate("text", label = "L548", x = 2, y = 1, size = 5)+
+  ylim(c(0,1))#Anatone: 84 Weird
 plot(genomic_data$Loc618) #146, 159, 165, 168, 172, 178, 181, 184, 187, 190, 195, 236
-ggplot() + geom_locus(aes(x = Loc618, fill = Treatment), data = genomic_data) #Anatone: 178, 187 OK
+L618 <- ggplot() + geom_locus(aes(x = Loc618, fill = Treatment), data = genomic_data)+
+  annotate("text", label = "L618", x = 2, y = 1, size = 5)+
+  ylim(c(0,1))#Anatone: 178, 187 OK
 plot(genomic_data$Loc831) #235, 238, 241, 252, 255, 258, 261, 264, 267, 272
-ggplot() + geom_locus(aes(x = Loc831, fill = Treatment), data = genomic_data) #Anatone: 252, 261 OK
+L831 <- ggplot() + geom_locus(aes(x = Loc831, fill = Treatment), data = genomic_data)+
+  annotate("text", label = "L831", x = 2, y = 1, size = 5)+
+  ylim(c(0,1))#Anatone: 252, 261 OK
+
+ggarrange(L025, L040, L209, L262, L307, L338, L396, L548, L618, L831,
+          ncol = 2, nrow = 5, common.legend = TRUE)
 
 freqs.loci.strata <- frequencies(genomic_data, stratum = "Treatment")
 ggplot(freqs.loci.strata) +
